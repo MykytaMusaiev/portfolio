c569d86a9e0798121caf85be5927fbe08359002a
Nikita Musaiev <n.musaiev@gmail.com>
2023-03-05 11:21:21 +0200

    6SIusQ8p: finished styling; finished responsibility; added TODO;

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {PluralSightLoginClone, ResponsivePhotographyWebsite} from "./projects";
 function App() {
   return (
       <>
+          {/*TODO: jkvTRBrP_ make routing system*/}
           <ResponsivePhotographyWebsite/>
           {/*<PluralSightLoginClone/>*/}
       </>
